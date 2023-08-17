6034f8e4d2d6b656d2f91a9e9909df7f2472d78e
Andre Ke <84885039+nohyp3@users.noreply.github.com>
2023-08-16 23:49:44 -0400

    Create read_now.py

---
+++ read_now.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import json
+import bme680
+import time
+from datetime import datetime
+#add the tiny db database 
+from tinydb import TinyDB, Query
+db = TinyDB('db.json')
+current_time = datetime.now()
+json_str = json.dumps({current_time},default=str)
+print("""read-all.py - Displays temperature, pressure, humidity, and gas.
+
+Press Ctrl+C to exit!
+
+""")
+
+try:
+    sensor = bme680.BME680(bme680.I2C_ADDR_PRIMARY)
+except (RuntimeError, IOError):
+    sensor = bme680.BME680(bme680.I2C_ADDR_SECONDARY)
+
+# These calibration data can safely be commented
+# out, if desired.
+
+print('Calibration data:')
+for name in dir(sensor.calibration_data):
+
+    if not name.startswith('_'):
+        value = getattr(sensor.calibration_data, name)
+
+        if isinstance(value, int):
+            print('{}: {}'.format(name, value))
+
+# These oversampling settings can be tweaked to
+# change the balance between accuracy and noise in
+# the data.
+
+sensor.set_humidity_oversample(bme680.OS_2X)
+sensor.set_pressure_oversample(bme680.OS_4X)
+sensor.set_temperature_oversample(bme680.OS_8X)
+sensor.set_filter(bme680.FILTER_SIZE_3)
+sensor.set_gas_status(bme680.ENABLE_GAS_MEAS)
+
+print('\n\nInitial reading:')
+for name in dir(sensor.data):
+    value = getattr(sensor.data, name)
+
+    if not name.startswith('_'):
+        print('{}: {}'.format(name, value))
+
+sensor.set_gas_heater_temperature(320)
+sensor.set_gas_heater_duration(150)
+sensor.select_gas_heater_profile(0)
+
+# Up to 10 heater profiles can be configured, each
+# with their own temperature and duration.
+# sensor.set_gas_heater_profile(200, 150, nb_profile=1)
+# sensor.select_gas_heater_profile(1)
+def get_now():
+    if sensor.get_sensor_data():
+        return {'time':json_str,'temperature':sensor.data.temperature, 'pressure':sensor.data.pressure, 'humidity':sensor.data.humidity,'gas-resistance':sensor.data.gas_resistance}
+                    
+    else:
+        return "null"
+        
