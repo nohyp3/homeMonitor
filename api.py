341077ca8d70e1aca2126fb72286733c3cd1200b
Andre Ke <84885039+nohyp3@users.noreply.github.com>
2023-08-16 23:50:26 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
 from tinydb import TinyDB, Query
 from flask_cors import CORS
+#import the script from within the same directory 
+import read_now 
 app = Flask(__name__)
 #need to add CORS compatibility for browser
 CORS(app)
@@ -10,6 +12,11 @@ SensorData = Query()
 @app.route('/get_sensor_data', methods=['GET'])
 def get_sensor_data():
 	sensor_data = db.all()
+	return jsonify(sensor_data)
+
+@app.route('/get_current_data', methods=['GET'])
+def get_current_data():
+	sensor_data = read_now.get_now() #call the get_now function 
 	return jsonify(sensor_data)
 	
 if __name__ == '__main__':
